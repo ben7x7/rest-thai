d09bd1019a30e58081fe048d2b87efe8dce38d74
benoit bessiere <benoit.bessiere@gmail.com>
2019-04-12 00:53:46 +0200

    add model + controller reservation

---
+++ app/models/reservation.rb
@@ -0,0 +1,19 @@
+class Reservation < MailForm::Base
+  attribute :name,      :validate => true
+  attribute :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
+  attribute :phone,     :validate => true
+  attribute :pax,       :validate => true
+  attribute :date,      :validate => true
+  attribute :time,      :validate => true
+  attribute :nickname,  :captcha  => true
+
+  # Declare the e-mail headers. It accepts anything the mail method
+  # in ActionMailer accepts.
+  def headers
+    {
+      :subject => "New reservation",
+      :to => "benoit.bessiere@me.com",
+      :from => %("#{name}" <#{email}>)
+    }
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-
+  resources :reservations, only: [:new, :create]
   root 'pages#home'
 end
