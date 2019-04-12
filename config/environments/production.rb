e77b6290874eb23e9dcedbeb5c8899a6a1b6bba0
benoit bessiere <benoit.bessiere@gmail.com>
2019-04-12 09:42:57 +0200

    change setting in prod env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,10 +65,9 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = { host: 'https://rest-thai.herokuapp.com'}
   config.action_mailer.delivery_method = :smtp
-
-  ActionMailer::Base.smtp_settings = {
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.smtp_settings = {
     :user_name => ENV['SENDGRID_USERNAME'],
     :password => ENV['SENDGRID_PASSWORD'],
     :domain => 'heroku.com',
