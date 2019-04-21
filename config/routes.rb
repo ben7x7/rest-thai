890f89efc8f9cf48c19db5e60474b256b4c6821e
benoit bessiere <benoit.bessiere@gmail.com>
2019-04-21 15:36:40 +0200

    setup I18n

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-
-  root 'pages#home'
-
-  get 'lamaisonthai', to: 'pages#lamaisonthai', as: :lamaisonthai
-
-  resources :reservations, only: [:new, :create]
-
+  scope '(:locale)', locale: /fr|en/ do
+    root 'pages#home'
+    get 'lamaisonthai', to: 'pages#lamaisonthai', as: :lamaisonthai
+    resources :reservations, only: [:new, :create]
+  end
 end
