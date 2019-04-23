befe3b6c4ceb5e4e334343d183372bd22d6f5980
benoit bessiere <benoit.bessiere@gmail.com>
2019-04-23 08:58:15 +0200

    add meta datas and favicon

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -12,7 +12,7 @@ class Reservation < MailForm::Base
   def headers
     {
       :subject => "New reservation",
-      :to => "benoit.bessiere@me.com, contact@tabledesartisans.ch",
+      :to => "contact@tabledesartisans.ch",
       :from => %("#{name}" <#{email}>)
     }
   end
