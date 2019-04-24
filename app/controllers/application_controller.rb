b536da199854c06e94dc051762db279d87b2283d
benoit bessiere <benoit.bessiere@gmail.com>
2019-04-24 11:34:41 +0200

    translate lmt menu

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,13 @@
 class ApplicationController < ActionController::Base
    before_action :set_locale
 
+   def default_url_options(options = {})
+    { locale: I18n.locale }
+  end
+
   def set_locale
     I18n.locale = params[:locale] || I18n.default_locale
   end
 
-  def default_url_options(options = {})
-    { locale: I18n.locale }.merge options
-  end
+
 end
