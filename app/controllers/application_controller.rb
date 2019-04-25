5919143bb00f4ecf5d402f4c7fd347057ff72651
benoit bessiere <benoit.bessiere@gmail.com>
2019-04-25 08:30:02 +0200

    change I18n parameters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,11 @@
 class ApplicationController < ActionController::Base
-   before_action :set_locale
-
-   def default_url_options(options = {})
-    { locale: I18n.locale }
-  end
+  before_action :set_locale
 
   def set_locale
-    I18n.locale = params[:locale] || I18n.default_locale
+    I18n.locale = params.fetch(:locale, I18n.default_locale).to_sym
   end
 
-
+  def default_url_options
+    { locale: I18n.locale == I18n.default_locale ? nil : I18n.locale }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-  scope ':locale', locale: /#{I18n.available_locales.join("|")}/ do
-    root 'pages#home'
+  scope '(:locale)', locale: /fr|en/ do
+    root to: 'pages#home'
     get 'lamaisonthai', to: 'pages#lamaisonthai', as: :lamaisonthai
     resources :reservations, only: [:new, :create]
   end
-  match '*path',via: [:get, :post], to: redirect("/#{I18n.default_locale}/%{path}")
-  match '',via: [:get, :post], to: redirect("#{I18n.default_locale}")
 end
