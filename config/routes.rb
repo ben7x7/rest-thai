fc2f3f11869fb9d4a494e9177bdf67fe67492738
benoit bessiere <benoit.bessiere@gmail.com>
2020-12-02 14:21:00 +0100

    add gem cookies

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   scope '(:locale)', locale: /fr|en/ do
     root to: 'pages#home'
     get 'lamaisonthai', to: 'pages#lamaisonthai', as: :lamaisonthai
+    get 'cookies', to: 'pages#cookies', as: :cookies
     get 'mentions', to: 'pages#mentions', as: :mentions
     get 'confidentialite', to: 'pages#confidentialite', as: :confidentialite
     resources :reservations, only: [:new, :create]
