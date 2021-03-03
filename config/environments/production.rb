f2597eaec146fdda65e050dd59bb1c79000cbd3b
benoit bessiere <benoit.bessiere@gmail.com>
2021-03-03 19:30:46 +0100

    bundle update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,10 +68,10 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
-    :user_name => ENV['SENDGRID_USERNAME'],
-    :password => ENV['SENDGRID_PASSWORD'],
+    :user_name => 'apikey',
+    :password => ENV['SENDGRID_API_KEY'],
     :domain => 'heroku.com',
-    :address => 'smtp.sendgrid.net',
+    :server => 'smtp.sendgrid.net',
     :port => 587,
     :authentication => :plain,
     :enable_starttls_auto => true
